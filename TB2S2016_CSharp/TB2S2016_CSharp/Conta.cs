9c0709dfee02b6ccc3928582323c31d7bbedad88
aclopesjr <aclopesjr@hotmail.com>
2016-10-17 20:52:44 -0200

    First Commit

---
+++ TB2S2016_CSharp/TB2S2016_CSharp/Conta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB2S2016_CSharp
+{
+    class Conta
+    {
+        string agencia;
+        String numero;
+        String nome;
+        DateTime dataAbertura;
+        double saldo;
+        Double limite;
+    }
+}
