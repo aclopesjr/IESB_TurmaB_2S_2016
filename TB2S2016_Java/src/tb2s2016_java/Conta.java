d2755a456d274425469c8ab386fed0f9b6d625aa
aclopesjr <aclopesjr@hotmail.com>
2016-10-17 20:52:44 -0200

    First Commit

---
+++ TB2S2016_Java/src/tb2s2016_java/Conta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tb2s2016_java;
+
+import java.util.Date;
+
+/**
+ *
+ * @author antonio.lopes
+ */
+public class Conta {
+ 
+    String agencia;
+    String numero;
+    String nome;
+    Date dataAbertura;
+    Double saldo;
+    Double limite;
+}
